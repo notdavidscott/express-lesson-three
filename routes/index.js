3b9ee4b3f23c8feb9f61ffa445961076448e8b11
David Scott Perez <david.scott.perez@live.com>
2018-07-17 15:03:04 -0700

    update #1
    added model and /:id routes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var users = require('../models/users');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/person/:id', function(req, res, next) {
+  let person = users.people.find(peep => {
+    return peep.id === parseInt(req.params.id);
+  });
+  res.render('index', { person });
+  console.log(req.url);
 });
+
 
 module.exports = router;
